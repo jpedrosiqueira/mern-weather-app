efeeb6975fe5a5c0c79afc8c0a840b0f923f354d
Pedro Siqueira <johnpedrosiqueira@gmail.com>
2021-09-23 20:26:04 -0700

    [Server] Create an express router to be split from the server, adding a route

---
+++ server/routes/api/searchLocation.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const router = express.Router();
+const axios = require("axios");
+const dotenv = require("dotenv");
+dotenv.config();
+
+router.post("/search-location", (req, res) => {
+  const locationName = req.body.cityName;
+  const unit = req.body.unit;
+
+  const apiKey = `&appid=${process.env.WEATHER_KEY}`;
+  const unitsFormat = `&units=${unit}`;
+
+  // First API URL is to fetch data based on the city location from the body request,
+  // that will be used to get coordinates and call a second API endpoint.
+  const baseUrl1 = "http://api.openweathermap.org/data/2.5/weather?q=";
+  const apiUrl1 = baseUrl1 + locationName + apiKey + unitsFormat;
+
+  axios
+    .get(apiUrl1)
+    .then((currentWeatherResponse) => {
+      const coordinates = currentWeatherResponse.data.coord;
+      const baseUrl2 = "https://api.openweathermap.org/data/2.5/onecall?";
+      const part = "minutely,alerts";
+      const coord = `lat=${coordinates.lat}&lon=${coordinates.lon}&exclude=${part}`;
+
+      // The second API URL we use the coordinates obtained from first API URL
+      // to get latitude and longitude, which we use for this endpoint.
+      // Based on lat and lon, we can get the forecast data, plus current weather data.
+      const apiUrl2 = baseUrl2 + coord + apiKey + unitsFormat;
+
+      axios.get(apiUrl2).then((forecastResponse) => {
+        // Here we are first combining the data from first and second API URLs,
+        // then we send the response.
+        const allData = {
+          currentWeatherData: currentWeatherResponse.data,
+          forecastData: forecastResponse.data,
+        };
+        res.json(allData);
+      });
+    })
+    .catch((error) => {
+      console.log("error is " + error);
+    });
+});
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,33 +1,7 @@
-// const express = require("express");
-// const cors = require("cors");
-// const app = express();
-// const port = 5000;
-
-// app.use(cors());
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
-
-// // Use API routes from the api folder
-// app.use("/routes/api", require("./routes/api"));
-
-// app.get("/", (req, res) => {
-//   res.send("PORT 5000");
-// });
-
-// app.listen(port, (err) => {
-//   if (err) {
-//     console.log(err);
-//   }
-//   console.log("Listening on port " + port);
-// });
-
 const express = require("express");
 const cors = require("cors");
-require("dotenv").config({ path: "./../../../.env" });
 const app = express();
-const axios = require("axios");
-const dotenv = require("dotenv");
-dotenv.config();
+const searchLocationRoute = require("./routes/api/searchLocation");
 
 app.use(cors());
 app.use(express.json());
@@ -37,35 +11,7 @@ app.get("/", (req, res) => {
   res.send("This is a weather app server");
 });
 
-app.post("/search-location", (req, res) => {
-  const zipCode = req.body.zipCodeInput;
-  const unit = req.body.unit;
-
-  const apiKey = `&appid=${process.env.WEATHER_KEY}`;
-  const unitsFormat = `&units=${unit}`;
-  const baseUrl1 = "http://api.openweathermap.org/data/2.5/weather?zip=";
-  const baseUrl2 = "https://api.openweathermap.org/data/2.5/onecall?";
-  const part = "minutely,alerts";
-  const apiUrl1 = baseUrl1 + zipCode + apiKey + unitsFormat;
-  axios
-    .get(apiUrl1)
-    .then((currentWeatherResponse) => {
-      const coordinates = currentWeatherResponse.data.coord;
-      const coord = `lat=${coordinates.lat}&lon=${coordinates.lon}&exclude=${part}`;
-      const apiUrl2 = baseUrl2 + coord + apiKey + unitsFormat;
-
-      axios.get(apiUrl2).then((forecastResponse) => {
-        const allData = {
-          currentWeatherData: currentWeatherResponse.data,
-          forecastData: forecastResponse.data,
-        };
-        res.json(allData);
-      });
-    })
-    .catch((error) => {
-      console.log("error is " + error);
-    });
-});
+app.use(searchLocationRoute);
 
 app.listen(port, () => {
   console.log(`App running on port ${port} `);
