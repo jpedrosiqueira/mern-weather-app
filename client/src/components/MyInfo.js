0576598800a1725e2edd1a3149abad6d98859821
Pedro Siqueira <johnpedrosiqueira@gmail.com>
2023-06-28 10:26:37 -0700

    add noopener to <a> tags with rel

--- client/src/components/MyInfo.js
+++ client/src/components/MyInfo.js
@@ -6,20 +6,24 @@ export const MyInfo = () => {
     <div className="info-container">
       <div>This web app was created by Pedro Siqueira.</div>
       <div className="social-links">
-        <a href="https://github.com/jpedrosiq" target="_blank" rel="noreferrer">
+        <a
+          href="https://github.com/jpedrosiq"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <i className="fa fa-github fa-2x"></i>
         </a>
         <a
           href="https://www.linkedin.com/in/jpedrosiqueira/"
           target="_blank"
-          rel="noreferrer"
+          rel="noopener noreferrer"
         >
           <i className="fab fa-linkedin fa-2x"></i>
         </a>
         <a
           href="mailto:johnpedrosiqueira@gmail.com"
           target="_blank"
-          rel="noreferrer"
+          rel="noopener noreferrer"
         >
           <i className="fa fa-envelope fa-2x"></i>
         </a>
